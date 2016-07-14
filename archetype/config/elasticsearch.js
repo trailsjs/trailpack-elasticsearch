5c956caf57272b97dc7d156d46632d15968b9083
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2016-07-14 17:32:15 +0300

    create elasticsearch connection

--- archetype/config/elasticsearch.js
+++ archetype/config/elasticsearch.js
@@ -5,4 +5,27 @@
  */
 module.exports = {
 
+  connection: {
+    /*
+    // Elastic hosts configs
+    hosts: [],
+    */
+
+    /**
+     * Elasticsearch host
+     */
+    host: 'localhost:9200',
+
+    /**
+     * Log level
+     */
+    log: 'trace'
+  },
+
+  /**
+   * If true trailpack will validate connection to elasticsearch
+   * @type {Boolean}
+   */
+  validateConnection: false
+
 }
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict'
 
 const _ = require('lodash')
-const lib = require('./lib')
 const Trailpack = require('trailpack')
+const elasticsearch = require('elasticsearch')
+
 /**
  * ElasticSearch integration for Trails.js
  */
@@ -11,29 +12,55 @@ module.exports = class ElasticsearchTrailpack extends Trailpack {
   /**
    * Ensure that this trailpack supports the configured migration
    */
-  validate () {
-    if (!this.app.config.elasticsearch) {
+  validate() {
+    if (!_.isObject(this.app.config.elasticsearch))
       return Promise.reject(new Error('No configuration found at config.elasticsearch !'))
+
+    if (!_.isObject(this.app.config.elasticsearch.connection))
+      return Promise.reject(new Error('No connection configuration defined !'))
+  }
+
+  configure() {
+    // setup default log level to warning
+    if (!this.app.config.elasticsearch.connection.log) {
+      this.app.config.elasticsearch.connection.log = 'warning'
     }
   }
 
-  configure () {
-  }
-
-  initialize () {
+  initialize() {
     super.initialize()
 
-    return Promise.resolve()
+    this.app.elasticClient = new elasticsearch.Client(this.app.config.elasticsearch.connection)
+
+    // If no need to validate connection - exit
+    if (!this.app.config.elasticsearch.validateConnection)
+      return Promise.resolve()
+
+    // validating connection using ping command
+    return new Promise((resolve, reject) => {
+      this.app.elasticClient.ping({
+        requestTimeout: 3000
+      }, function(err) {
+        if (err)
+          return reject(err)
+
+        resolve()
+      })
+    })
   }
 
   /**
    * Close connection to Elasticsearch
    */
-  unload () {
-    return Promise.resolve()
+  unload() {
+    if (!this.app.elasticClient || !_.isFunction(this.app.elasticClient.close))
+      return
+
+    // Closing elasticsearch connection
+    this.app.elasticClient.close()
   }
 
-  constructor (app) {
+  constructor(app) {
     super(app, {
       config: require('./config'),
       api: require('./api'),
